d3863872ecee762fdf661c9d3dce0d165eee5461
Sammy D <sammydowds1993@gmail.com>
2018-04-21 11:27:43 -0400

    Second revision

--- Resources/JavaScript/main.js
+++ Resources/JavaScript/main.js
@@ -1,34 +1,20 @@
 
 
-function lineFollowers(e) {
-	document.getElementById("horiz_follow").style.display = 'block';
-	document.getElementById("vertical_follow").style.display = 'block';
-	var x_ = e.clientX
-	var y_ = e.clientY
-	var x_off = x_ - 12;
-    var y_off = y_ - 20;
-    var h_line = document.getElementById("horiz_follow");
-    var v_line = document.getElementById("vertical_follow");
-    h_line.style.transform = "translate3d(" + (x_off) + "px, 0px, 0px)";
-    v_line.style.transform = "translate3d("+"0px," + (y_off) + "px, 0px)";
+var el = document.getElementById('blocktext')
+if(el) {
+    el.addEventListener('click', test);
+}
 
-
+function test() {
+    console.log('HELLLELLELLELELE');
 }
 
 function mouseFollowers(e) {
-	//x direction max = 0 - 337
-	var x_ = e.clientX
+    //x direction max = 0 - 337
     var x = e.clientX -(548);
-    var y_ = e.clientY
     //Y direction max = 0 - 337
-    var y = e.clientY - (178+63);
-   
+    var y = e.clientY - (241);
     var coord = document.getElementById("coord_tag");
-    
-    coord.style.transform = "translate3d(" + (x_) + "px," + y_ + "px, 0)";
-    
-
-
     var coor = "(" + x + "," + y + ")";
     document.getElementById("coord_tag").innerHTML = coor;
     
@@ -37,9 +23,52 @@ function mouseFollowers(e) {
 
 }
 
+function offset_tire_x(event) {
+
+    var x = e.clientX - 40;
+    return x
+
+}
+
+function offset_tire_y(e) {
+    var y = e.clientY - 40;
+    return y
+
+}
+
+function tire_hovering(event) {
+    document.getElementById("tire").style.display = 'block';
+    var tire_hov = document.getElementById("tire");
+
+    tire_hov.style.transform = "translate3d(" + offset_tire_x(e) + "px,"+ offset_tire_y(e) + "px, 0px)";
+
+    
+}
+
+
+
+// function lineFollowers(e) {
+// 	document.getElementById("horiz_follow").style.display = 'block';
+// 	document.getElementById("vertical_follow").style.display = 'block';
+// 	var x_ = e.clientX;
+// 	var y_ = e.clientY;
+// 	var x_off = x_ - 12;
+//     var y_off = y_ - 20;
+//     var h_line = document.getElementById("horiz_follow");
+//     var v_line = document.getElementById("vertical_follow");
+//     h_line.style.transform = "translate3d(" + (x_off) + "px, 0px, 0px)";
+//     v_line.style.transform = "translate3d("+"0px," + (y_off) + "px, 0px)";
+
+
+
+// }
+
+
+
 function clearCoor() {
     document.getElementById("coord_tag").innerHTML = "";
-    document.getElementById("vertical_follow").style.display = 'none';
-    document.getElementById("horiz_follow").style.display = 'none';
+    // document.getElementById("vertical_follow").style.display = 'none';
+    // // document.getElementById("horiz_follow").style.display = 'none';
+    document.getElementById("tire").style.display = 'none';
     
 }
