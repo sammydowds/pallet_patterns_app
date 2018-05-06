8a6f42aef6a4070fdc3a7f3b58933a2d36f079a2
Sammy D <sammydowds1993@gmail.com>
2018-05-06 12:05:55 -0400

    Fixed positional problems on pallet, as well as added global radius of tire to use

--- Resources/JavaScript/main.js
+++ Resources/JavaScript/main.js
@@ -1,29 +1,35 @@
 
+var radius = 70;
+var sidewall = 30;
+console.log(radius)
 
 function tire_hover(e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    var radius = 45; 
+    console.log(radius);
+    var x = e.pageX;
+    var y = e.pageY;
+   
+    
     var padding_tire = 10;
 
     var tire_position = document.getElementById("tire");
     var coordin_text = document.getElementById('coord_text');
     var loc_left = document.getElementById("Pallet").offsetLeft;
     var loc_top = document.getElementById("Pallet").offsetTop; 
+    
 
 
     //Updating coordinate text
-    console.log(document.getElementById("Pallet").offsetLeft);
-    var x_ = (e.pageX - 481);
-    var y_ = (e.pageY - 173);
-    console.log(x_, y_);
+    
+    var x_ = (e.pageX - loc_left + 170 + radius);
+    var y_ = (e.pageY - loc_top + 170 + radius);
+    
     coordin_text.innerHTML = "coordinates: " + (x_) + "," + (y_); 
     
     //updating tire location on the screen (hovering)
     
     tire_position.style.transform = "translate3d(" + (x)+ "px,"+ (y)+ "px,0px)"; 
     
-    console.log('Here is the mouse position: ' + x + ',' + y); 
+    
     
 
     
@@ -31,7 +37,7 @@ function tire_hover(e) {
 
 
 function put_tire(e) {
-    console.log('BUTTON CLICKED');
+   
     var x = e.clientX;
     var y = e.clientY;
 
@@ -47,17 +53,21 @@ function put_tire(e) {
 function pallet_pix_inches(e) {
     var width = document.getElementById("Width_Pallet").value; 
     var length = document.getElementById("Length_Pallet").value; 
-    var x_scale = (1/width)*344; 
-    var y_scale = (1/length)*344; 
+    var x_scale = (1/width)*340; 
+   
 
-    var tire_x = document.getElementById("OD").value * x_scale; 
-    var tire_y = document.getElementById("OD").value * y_scale; 
-    var side_wall = tire_x * 0.20; 
 
-    document.getElementById("tire").style.width = tire_x - (side_wall*2) + "px"; 
-    document.getElementById("tire").style.height = tire_y - (side_wall*2) + "px";  
-    document.getElementById("tire").style.border = side_wall + "px solid black";
-    var radius = 0.5*tire_x + side_wall + 10;
+    //How many pixels the tire needs to be to scale to the pallet pixels
+    var tire_pix_od = document.getElementById("OD").value * x_scale; 
+
+    //How many pixels in the vertical direction tire needs to be
+  
+
+    document.getElementById("tire").style.width = tire_pix_od - (sidewall*2) + "px"; 
+    document.getElementById("tire").style.height = tire_pix_od - (sidewall*2) + "px";  
+    console.log(tire_pix_od)
+    radius = 0.5*tire_pix_od + 10;
+    console.log(radius);
     
 
 }
